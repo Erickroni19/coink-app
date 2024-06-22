2c5fa41ac4dda26eb092e50ba5524d5b16d6267b
Erick Romero <erickromeronieto018@gmail.com>
2024-06-22 17:37:20 -0500

    refactorización de home-auth sass y inicio del layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: 'auth', loadChildren: () => import('./pages/pages.routes').then( c => c.PAGE_ROUTES)},
-  {path:'', redirectTo:'auth', pathMatch:'full'}
+  { path: 'auth', loadChildren: () => import('./pages/pages.routes').then( c => c.PAGE_ROUTES) },
+  { path:'', redirectTo:'auth', pathMatch:'full'},
+  { path: '**', redirectTo: 'auth' }
 ];
--- src/app/pages/home-auth-page/home-auth-page.component.ts
+++ src/app/pages/home-auth-page/home-auth-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home-auth-page',
   standalone: true,
-  imports: [IonicModule],
+  imports: [ IonicModule, RouterModule ],
   templateUrl: './home-auth-page.component.html',
   styleUrls: ['./home-auth-page.component.scss'],
 })
